77bbc68aa5d860a697a8587af2e1b41f98adf3f3
JOMILORO <103610359+JOMILORO@users.noreply.github.com>
2023-04-17 16:42:03 -0600

    Modificar archivo manifest_py por cambio de version a Odoo 15

--- __manifest__.py
+++ __manifest__.py
@@ -2,11 +2,11 @@
 {
     'name': 'Mantenimiento Confetex',
     'category': 'Manufacturing/Maintenance',
-    'version': '14.0.1',
+    'version': '15.0.1',
     'author': 'José Miguel López Roano',
     'summary': 'Módulo para ordenes de mantenimiento y control de máquinas y herramientas en grupo Confetex',
     'description': '''Modulo para un control de peticiones de mantenimiento y control de máquinas y herramienta con 
-                      impresión de código QR en Odoo14''',
+                      impresión de código QR en Odoo15''',
     'website': 'https://www.confetex.com',
     'depends': [
         'base',
